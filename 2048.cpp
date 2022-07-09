a6e01adf4fab971ffab721bb89ec71feb468c4e5
Eskil Refsgaard <eskl.refsgaard@gmail.com>
2022-07-09 23:54:12 +0200

    added menu

--- 2048.cpp
+++ 2048.cpp
@@ -15,12 +15,12 @@ using namespace std;
 class Game
 {
 private:
-    std::vector<int> _b; // 1-16 spaces.
+    std::vector <int> _b; // 1-16 spaces.
     std::string name;
 
-public:
-    Game(){}; // startup new game
-    void board();
+public: 
+    Game(){std::cout << "\nMade a new game\n";}; // startup new game
+    void board();   //skal brukes hver gang
 };
 
 std::map<std::string, Game *> games;
@@ -30,29 +30,30 @@ void g_show_games();
 void g_play();
 void old_g_start(); // start old game
 void board();
+void menu();
 
 // main program
 
 int main()
 {
     char input;
+    menu();
     input = r_char("Whitch operation do you want to execute");
+
     while (input != 'Q')
     {
         switch (toupper(input))
         {
         case 'N':
-            g_new();
-            std::cout << "fuck";
-            break;
+            g_new();            break;
+
         case 'S':
-            g_show_games();
-            break;
+            g_show_games();     break;
+
         case 'B':
-            board();
-            break;
-        // funksjoner som skal lages
-        default:
+            board();            break;
+
+        default:    //actually u retarded, du ser fuckings menyen.
             std::cout << "\nRetard\n";
             break;
         }
@@ -81,12 +82,10 @@ void g_new()
 
     n_game = new Game();
 
-    std::cout << "\n2";
     std::string name = r_string("The name for THIS game");
-    // games.insert(std::pair <std::string, Game*> (name, n_game));    //fuc you
+    games.insert(std::pair <std::string, Game*> (name, n_game));    //fuc you
 
     delete n_game;
-    std::cout << "\n3";
 
     return; // when quittig the game.
 }
@@ -122,4 +121,14 @@ void board(){       //finsished board, just need to insert the vector placements
     std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
     std::cout  << "| " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " |" << endl;
     std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
+}
+
+void menu(){
+    std::cout << "\nPossble operations:\n";
+    std::cout << "\n\tN - new game";
+    std::cout << "\n\tS - show running games";
+    std::cout << "\n\tB - show board";
+    std::cout << "\n\tP - play existing game";
+    std::cout << "\n\t";
+    std::cout << "\n\t";
 }
