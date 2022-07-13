a90d032999e1764cb5db926a114c238470499c23
Eskil Refsgaard <eskl.refsgaard@gmail.com>
2022-07-14 00:19:21 +0200

    v2 is old file, 2048.cpp is new, but still get segmentation fault (core dumped)

--- 2048.cpp
+++ 2048.cpp
@@ -1,169 +1,120 @@
-/*
- *  Lager spillet 024, ,,8
- *
- */
-
-#include <iostream>
 #include <iomanip>
-#include "E_func.h"
+#include <iostream>
+#include "E_func.h"     // Homemade
 #include <vector>
-#include <map>
-using namespace std;
 
-// functions
+/*
+    Class
+*/
 
-class Game
-{
-private:
-    std::vector <int> _b; // 1-16 spaces.
-    std::string name;
-    bool started;
+class Game{
+    private:
+        int board[16];    //b for board
+        std::string name;
 
-public: 
-    Game(); // startup new game, vil legge til nullstilling av spillbrett, får bare segmenation shit
-    void board();   //skal brukes hver gang
-    void play();
-    bool checkStart();
-    void gameStart(bool change);
-    void set_zero();
+    public:
+        //void readFile();
+        void reset();
+        void displayName();
+        void displayBoard();
+        void playGame();
+
 };
 
-std::map<std::string, Game *> games;
+/*
+    Decalation of Functions
+*/
 
-void g_new(); // starts new game
-void g_show_games();
-void g_play_game();
-void old_g_start(); // start old game
-void board();
 void menu();
+void new_game();
+void play_game();
+void show_games();
 
-// main program
+std::vector <Game*> gGames;
 
-int main()
-{
+int main(){
     char input;
+
+    // check from the files
+
     menu();
-    input = r_char("Whitch operation do you want to execute");
+    input = r_char("What operation do you want to execute");
 
-    while (input != 'Q')
-    {
-        switch (toupper(input))
-        {
-        case 'N':
-            g_new();            break;
-
-        case 'S':
-            g_show_games();     break;
-
-        case 'B':
-            board();            break;
-
-        case 'P':
-            g_play_game();           break;
-
-        default:    //actually u retarded, du ser fuckings menyen.
-            std::cout << "\nRetard\n";
-            break;
+    while(input != 'Q'){
+        std::cout << "\n" << input << "\n";
+        switch(input){
+            case 'N' : new_game();      break;
+            case 'P' : play_game();     break;
+            case 'S' : show_games();    break;
         }
+
         menu();
-        input = r_char("Whitch operation do you want to execute");
+        std::cout << "\ncheck2" << std::endl;
+        input = r_char("What operation do you want to execute");
     }
-    return 0;
 }
 
-// game func
-
-Game::Game(){
-    started = true;
-}
-
-bool Game::checkStart(){
-    return started;
-}
-
-void Game::play(){
-    for(int i = 0; i < 2; i++)
-        Game::board();
-}
-
-void Game::set_zero(){  // not work
+void Game::reset(){
+    this->name = r_string("What is the name of the game");
+    
     for(int i = 0; i < 16; i++){
-        _b[i] = 0;
+        board[i] = 0;
     }
 }
 
-// global funcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-void g_new()
-{
-
-    Game *n_game;
-
-    n_game = new Game();
-
-    std::string name = r_string("The name for THIS game");
-
-    n_game->set_zero();     //  notwork
-
-    games.insert(std::pair <std::string, Game*> (name, n_game));    //fuc you
-
-    delete n_game;
-
-    return; // when quittig the game.
+void Game::displayName(){
+    std::cout << this->name;
 }
 
-void g_show_games()
-{ // Viser alle spilles
-    int i = 1;
-    if (!games.empty())
-    {
-        for (const auto & val : games)
-        {
-            std::cout << i++ << ". " << val.first << std::endl;
-        }
+void Game::displayBoard(){
+    for(int i = 0; i < 16; i++){
+        std::cout << board[i] << " ";
+        if(i % 3 == 0) std::cout << "\n";
     }
-
-    else
-        std::cout << "\nNo games started.";
 }
 
-void g_play_game(){
-    bool input;
+void Game::playGame(){
+    displayBoard();
+}
 
-    if(!games.empty()){
-        g_show_games();
+/*
+    Functions, main
+*/
 
-        std::string name= r_string("Which game do you want to run: ");
-        for(auto & val : games){
+void new_game(){
 
-            if(name == val.first){
-                val.second->play();
-                    
-            }else
-                std::cout << "\nNo games with that name";
+    std::cout << "\nhey\n" << std::endl;
+    Game *temp = new Game();
+
+    std::cout << "\ncheck 1" << std::endl;
+
+    temp->reset();
+    gGames.push_back(temp);
+
+}
+
+void play_game(){
+    int nr;
+    if(!gGames.empty()){
+        show_games();
+        nr = r_int_bet("Whitch game do you want to play", 0, gGames.size());
+        if(nr){
+            gGames[nr]->playGame();
+        }else
+            std::cout << "\nNo games will be played!" << std::endl;
+
+    }else
+        std::cout << "No games to play" << std::endl;
+}
+
+// Maybe unnecessary
+void show_games(){
+    if(!gGames.empty()){
+        for(int i = 0; i < gGames.size(); i++){
+            std::cout << "\n" << i+1 << ". "; gGames[i]->displayName();   
         }
     }else
-        std::cout << "\nNo games have started.";
-}
-
-
-void Game::board()
-{
-    std::cout << "- - - - - - - - - - - - - - - -";
-    std::cout << "| " << _b[0] << " | " << _b[1] << " | " << _b[2] << " | " << _b[3] << " |";
-    //more
-}
-
-void board(){       //finsished board, just need to insert the vector placements. 
-    std::cout << "- - - - - - - - - - - - - - - - -, Dette er bare en visuell" << std::endl;
-    std::cout  << "| " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " |" << endl;
-    std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
-    std::cout  << "| " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " |" << endl;
-    std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
-    std::cout  << "| " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " |" << endl;
-    std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
-    std::cout  << "| " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " | " << std::setw(5) << "x" << " |" << endl;
-    std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
+        std::cout << "\nNo games to show" << std::endl;
 }
 
 void menu(){
--- E_func.h
+++ E_func.h
@@ -12,6 +12,7 @@ const int MAXCHAR = 200;
 char r_char(std::string txt);
 std::string r_string(std::string txt);
 int r_int(std::string txt);
+int r_int_bet(std::string txt, int min, int max);
 bool y_or_no(std::string txt);
 
 char r_char(std::string txt){
@@ -24,7 +25,7 @@ char r_char(std::string txt){
 //This just kinda works 
 std::string r_string(std::string txt){
     std::string name;
-    std::cout << std::endl << txt << " :";
+    std::cout << std::endl << txt << ": ";
     getline(std::cin, name);
     return name;
 }
@@ -33,6 +34,15 @@ int r_int(std::string txt){
     int nr;
     std::cout << std::endl << txt << ":";
     std::cin >> nr; std::cin.ignore(MAXCHAR, '\n');
+    return nr;
+}
+
+int r_int_bet(std::string txt, int min, int max){
+    int nr;
+    while(nr < min || nr > max){
+        std::cout << std::endl << txt << "(" << min << " - " << max << "): ";
+        std::cin >> nr; std::cin.ignore(MAXCHAR, '\n');
+    }
     return nr;
 }
 
