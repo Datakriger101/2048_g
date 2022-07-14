10f2ebb3e6d6feb04686915dc32cde7c5bf059ea
Eskil Refsgaard <eskl.refsgaard@gmail.com>
2022-07-15 00:59:22 +0200

    progress, added random number when choosing a direction, some output fixes

--- 2048.cpp
+++ 2048.cpp
@@ -1,26 +1,36 @@
 #include <iomanip>
 #include <iostream>
-#include "E_func.h"     // Homemade
+#include "E_func.h" // Homemade
 #include <vector>
+
+//test
+#include <cstdlib>   // for rand() func
 
 /*
     Class
 */
 
-class Game{
+class Game
+{
     private:
-        std::vector <int> b;
+        std::vector<int> b;
         std::string name;
+        bool lost;
 
     public:
-        //void readFile();
+        // void readFile();
         void startup();
         void displayName();
         void displayBoard();
         void playGame();
 
-        Game();
+        void moveMenu();
+        void newRandNumber();
+        bool checkValidMove(char tegn);
+        bool checkLoss();
+        // void movePlay();
 
+        Game();
 };
 
 /*
@@ -32,9 +42,10 @@ void new_game();
 void play_game();
 void show_games();
 
-std::vector <Game*> gGames;
+std::vector<Game *> gGames;
 
-int main(){
+int main()
+{
     char input;
 
     // check from the files
@@ -42,12 +53,15 @@ int main(){
     menu();
     input = r_char("What operation do you want to execute");
 
-    while(input != 'Q'){
-        std::cout << "\n" << input << "\n";
-        switch(input){
-            case 'N' : new_game();      break;
-            case 'P' : play_game();     break;
-            case 'S' : show_games();    break;
+    while (input != 'Q')
+    {
+        std::cout << "\n"
+                  << input << "\n";
+        switch (input)
+        {
+        case 'N' : new_game();      break;
+        case 'P' : play_game();     break;
+        case 'S' : show_games();    break;
         }
 
         menu();
@@ -56,24 +70,79 @@ int main(){
     }
 }
 
-Game::Game(){
+Game::Game()
+{
 
     this->name = r_string("What is the name of the game");
-    for(int i = 0; i < 16; i++){
+    this->lost = false;
+    for (int i = 0; i < 16; i++)
+    {
         b.push_back(0);
     }
 }
 
-void Game::startup(){   //in use??
-
-}
-
-void Game::playGame(){
+void Game::playGame()
+{
+    if(!lost){
+        displayBoard();
+        moveMenu();
     
-    displayBoard();
+    //newRandNumber();
+
+        char input = r_char("What operation u want to do");
+        while (input != 'Q')
+        {
+            //legge til at hvis ingen posisjoner er 0, så vill spilles avsluttes.
+
+            if (checkValidMove(input))
+            {
+                newRandNumber();
+                displayBoard();
+                std::cout << "\nValid - " << input << "\n";
+
+            }
+            else
+                std::cout << "\nInvalid input\n";
+            
+            if(checkLoss()){lost = true; break;}
+
+            input = r_char("What operation u want to do");
+
+        }
+    }else
+        std::cout << "\nThe has ended!" << std::endl;
+        displayBoard();
 }
 
-void Game::displayBoard(){
+bool Game::checkLoss(){
+    int ant = 0;
+    for(int i = 0; i < 16; i++){
+        if(b[i] != 0)ant++;
+    }
+    //std::cout << "\n - " << ant << " - \n";
+
+    return(ant == 16 ? true : false);
+}
+
+void Game::newRandNumber()
+{
+    int pos, nr;
+
+    do{
+        pos = rand() % 16;
+        std::cout << "\n -- " << pos << " --\n";
+    }while(b[pos] != 0);
+    nr = rand() % 2;
+    b[pos] = (nr % 2 == 1) ? 2 : 4;
+}
+
+bool Game::checkValidMove(char t)
+{
+    return ((t == 'W' || t == 'A' || t == 'S' || t == 'D') ? true : false);
+}
+
+void Game::displayBoard()
+{
     std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
     std::cout << "| " << std::setw(5) << b[0] << " | " << std::setw(5) << b[1] << " | " << std::setw(5) << b[2] << " | " << std::setw(5) << b[3] << " |" << std::endl;
     std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
@@ -85,59 +154,69 @@ void Game::displayBoard(){
     std::cout << "- - - - - - - - - - - - - - - - -" << std::endl;
 }
 
-/*
-void Game::n_displayBoard(){
-
-}
-*/
-
-
-
-void Game::displayName(){
+void Game::displayName()
+{
     std::cout << this->name;
+}
+
+void Game::moveMenu()
+{
+    std::cout << "\nup - W | left - A | down - S | D - right";
 }
 
 /*
     Functions, main
 */
 
-void new_game(){
+void new_game()
+{
 
-    std::cout << "\nhey\n" << std::endl;
+    std::cout << "\nhey\n"
+              << std::endl;
     Game *new_G = new Game();
 
     std::cout << "\ncheck 1" << std::endl;
 
-    //temp->startup();
+    // temp->startup();
     gGames.push_back(new_G);
-
 }
 
-void play_game(){
+void play_game()
+{
     int nr;
-    if(!gGames.empty()){
+    if (!gGames.empty())
+    {
         show_games();
         nr = r_int_bet("Whitch game do you want to play", 0, gGames.size());
-        if(nr){
-            gGames[nr-1]->playGame();
-        }else
+        if (nr)
+        {
+            gGames[nr - 1]->playGame();
+        }
+        else
             std::cout << "\nNo games will be played!" << std::endl;
-
-    }else
+    }
+    else
         std::cout << "No games to play" << std::endl;
 }
 
 // Maybe unnecessary
-void show_games(){
-    if(!gGames.empty()){
-        for(int i = 0; i < gGames.size(); i++){
-            std::cout << "\n" << i+1 << ". "; gGames[i]->displayName();   
+void show_games()
+{
+    if (!gGames.empty())
+    {
+        for (int i = 0; i < gGames.size(); i++)
+        {
+            std::cout << "\n"
+                      << i + 1 << ". ";
+            gGames[i]->displayName();
         }
-    }else
+    }
+    else
         std::cout << "\nNo games to show" << std::endl;
 }
 
-void menu(){
+void menu()
+{
     std::cout << "\nPossble operations:\n";
     std::cout << "\n\tN - new game";
     std::cout << "\n\tS - show running games";
--- test.cpp
+++ test.cpp
@@ -4,17 +4,42 @@
  */
 
 #include <iostream>
+#include <iomanip>
 #include "E_func.h"
 #include <vector>
 #include <map>
+
+#include <cstdlib>
 using namespace std;
 
-//functions
+//functions, yes
 
 int main(){
-    char tegn; 
 
-    cin >> tegn; 
-    cout << "\n" << tegn << "\n";
+    int nr;
+    int antall1[16];
+    int antall2[16];
+
+
+
+    for(int i = 0; i < 16; i++){
+        antall1[i] = 0;
+        antall2[i] = 0;
+    }
+
+    for(int i = 0; i < 1000; i++){
+        nr = rand() % 16;
+        antall1[nr] += 1;
+    }
+
+    for(int i = 0; i < 1000; i++){
+        nr = rand() % 16;
+        antall2[nr] += 1;
+    }
+
+    for(int i = 0; i < 16; i++){
+        cout << setw(2) << i << ". " << antall1[i] << "\t\t" << setw(2) << i << ". " << antall2[i] << "\n";
+    }
+
     //Conclusion, can not use arrow keys with terminal. fuck
 }
